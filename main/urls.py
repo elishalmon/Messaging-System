8bc0fd56923b5ebafd9c06257ac25379727ec697
elishalmon <elishalmon111@gmail.com>
2021-06-13 20:35:54 +0300

    modify urls pathes to be more seperated

---
+++ main/urls.py
@@ -0,0 +1,36 @@
+from main.views import MessageView
+from django.urls import path
+
+messages_list = MessageView.as_view({
+    'get': 'list',
+    'get': 'unread',
+})
+
+create_message = MessageView.as_view({
+    'post': 'create',
+})
+
+get_message = MessageView.as_view({
+    'get': 'retrieve',
+})
+
+
+delete_outbox_message = MessageView.as_view({
+    'delete': 'delete_outbox_message',
+})
+
+delete_inbox_message = MessageView.as_view({
+    'delete': 'delete_inbox_message',
+})
+
+urlpatterns = (
+    path('', messages_list, name='messages'),
+    path('unread/', messages_list, name='messages-unread'),
+    path('create/', create_message, name='messages-create'),
+    path('<int:pk>/', get_message, name='read-message'),
+    path('delete/inbox/<int:pk>/', delete_inbox_message, name='delete-inbox-message'),
+    path('delete/outbox/<int:pk>/', delete_outbox_message, name='delete-outbox-message'),
+)
+
+
+
--- main/views.py
+++ main/views.py
@@ -14,14 +14,19 @@ class MessageView(viewsets.ModelViewSet):
         return GetMessageSerializer
 
     def get_queryset(self):
+        """
+            Return the corresponding queryset (according to the action) ordered by the creation time
+        """
         user = self.request.user
         queryset = (Message.objects.filter(reciever=user).filter(deleted_by_reciever=False) |
                     Message.objects.filter(sender=user).filter(deleted_by_sender=False))
-        if self.action in ('list', 'unread', ):
+        if self.action in ('list', 'unread', 'delete_inbox_message'):
             queryset = queryset.filter(reciever=user).filter(deleted_by_reciever=False)
             if self.action in ('unread', ):
                 queryset = queryset.filter(is_read=False)
-        return queryset
+        if self.action in ('delete_outbox_message', ):
+            queryset = queryset.filter(sender=user).filter(deleted_by_sender=False)
+        return queryset.order_by('-created_at')
 
     def create(self, request):
         serializer = CreateMessageSerializer(data=request.data)
@@ -44,13 +49,11 @@ class MessageView(viewsets.ModelViewSet):
     def retrieve(self, request, pk):
         """
             Returns a single message (optional from both inbox or outbox) according to the pk argument,
-            if the user ask for income message, and its 'unread'
+            if the user ask for income message, and its marked as'unread'
             then update is_read flag to true.
         """
         user = request.user
         try:
-            # message = (Message.objects.filter(reciever=user).filter(deleted_by_reciever=False) |
-            #            Message.objects.filter(sender=user).filter(deleted_by_sender=False)).get(id=pk)
             message = self.get_queryset().get(id=pk)
         except Message.DoesNotExist:
             return Response({"detail": "Not found."}, status=status.HTTP_404_NOT_FOUND)
@@ -60,32 +63,43 @@ class MessageView(viewsets.ModelViewSet):
         serializer = GetMessageSerializer(message)
         return Response(serializer.data)
 
-    @action(detail=False)
+    @action(detail=False, methods=['get'])
     def unread(self, request):
+        print('unread')
         queryset = self.get_queryset()
         serializer = GetMessageSerializer(queryset, many=True)
         return Response(serializer.data)
 
-    def destroy(self, request, pk):
-        """
-            This function marks the 'delete_by_sender/reciever' flag to true according to the user and the pk argument,
-            (optional from both inbox or outbox)
-            if the message was marked from both sides as deleted so it will delete also from database
-        """
-        user = request.user
+    """
+        Delete message section - each function mark the 'delete_by_sender/reciever' flag to true
+        if the message was marked as deleted also in the other side
+        then it will deleted from the database too.
+    """
+
+    @action(detail=True, methods=['delete'])
+    def delete_inbox_message(self, request, pk):
         try:
             message = self.get_queryset().get(id=pk)
         except Message.DoesNotExist:
             return Response({"detail": "Not found."}, status=status.HTTP_404_NOT_FOUND)
-
-        if user.id == message.reciever.id:
-            message.deleted_by_reciever = True
-            message.save()
-            if message.deleted_by_sender:
-                message.delete()
-        else:
-            message.deleted_by_sender = True
-            message.save()
-            if message.deleted_by_reciever:
-                message.delete()
+        message.deleted_by_reciever = True
+        message.save()
+        if message.deleted_by_sender:
+            message.delete()
         return Response({"detail": "Message deleted successfully"}, status=status.HTTP_200_OK)
+
+    @action(detail=True, methods=['delete'])
+    def delete_outbox_message(self, request, pk):
+        try:
+            message = self.get_queryset().get(id=pk)
+        except Message.DoesNotExist:
+            return Response({"detail": "Not found."}, status=status.HTTP_404_NOT_FOUND)
+        message.deleted_by_sender = True
+        message.save()
+        if message.deleted_by_reciever:
+            message.delete()
+        return Response({"detail": "Message deleted successfully"}, status=status.HTTP_200_OK)
+
+
+
+
