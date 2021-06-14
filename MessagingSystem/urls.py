ae93cbc94ba3e67fc990afa9eb06600f1ae6afa9
elishalmon <elishalmon111@gmail.com>
2021-06-14 14:45:04 +0300

    fix unread url view issue

--- MessagingSystem/urls.py
+++ MessagingSystem/urls.py
@@ -20,10 +20,11 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('messages/', include('main.urls'))
+    path('messages/', include('main.urls')),
 ]
 
--- main/urls.py
+++ main/urls.py
@@ -3,8 +3,10 @@ from django.urls import path
 
 messages_list = MessageView.as_view({
     'get': 'list',
-    'get': 'unread',
+    #'get': 'unread',
 })
+
+unread_messages = MessageView.as_view({'get': 'unread'})
 
 create_message = MessageView.as_view({
     'post': 'create',
@@ -25,7 +27,7 @@ delete_inbox_message = MessageView.as_view({
 
 urlpatterns = (
     path('', messages_list, name='messages'),
-    path('unread/', messages_list, name='messages-unread'),
+    path('unread/', unread_messages, name='messages-unread'),
     path('create/', create_message, name='messages-create'),
     path('<int:pk>/', get_message, name='read-message'),
     path('delete/inbox/<int:pk>/', delete_inbox_message, name='delete-inbox-message'),
